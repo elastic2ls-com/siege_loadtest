76eed036fd77a484ca93485e0e29ba8f725bb7ee
Jonathan Costello <jon@weebly.com>
2015-01-07 15:28:31 -0800

    Updated entry.sh to allow for userlogin

--- entry.sh
+++ entry.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
+# function to urlencode username
+rawurlencode() {
+  local string="${1}"
+  local strlen=${#string}
+  local encoded=""
+
+  for (( pos=0 ; pos<strlen ; pos++ )); do
+     c=${string:$pos:1}
+     case "$c" in
+        [-_.~a-zA-Z0-9] ) o="${c}" ;;
+        * )               printf -v o '%%%02x' "'$c"
+     esac
+     encoded+="${o}"
+  done
+  echo "${encoded}"
+}
+
+# variables
 IFS="|"
 URLS=($URL_LIST)
 HOSTS=($ETC_HOSTS)
+USERNAME_ENCODED=$( rawurlencode "${USERNAME}" )
+LOGIN_URL_ENCODED=$( echo ${LOGIN_URL} | sed -e "s/{{USERNAME}}/${USERNAME_ENCODED}/g" | sed -e "s/{{PASSWORD}}/${PASSWORD}/g" )
 
 #CONFIGURE SIEGE SETTINGS
-echo "login-url = ${DOMAIN}${LOGIN_URL}"
-echo "login-url = ${DOMAIN}$LOGIN_URL" >> /etc/siege/siegerc
+echo "login-url = ${DOMAIN}${LOGIN_URL}" >> /etc/siege/siegerc
 echo "user-agent = Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0" >> /etc/siege/siegerc
-echo "username = ${USERNAME}" >> /etc/siege/siegerc
-echo "password = ${PASSWORD}" >> /etc/siege/siegerc
-#echo "follow-location = false" >> /etc/siege/siegerc
+echo "follow-location = false" >> /etc/siege/siegerc
 echo "internet = true" >> /etc/siege/siegerc
 echo "delay = 3" >> /etc/siege/siegerc
-echo "show-logfile = false" >> /etc/siege/siegerc
+cat /etc/siege/siegerc | grep -v '^#'
 
 #Add urls to urls.txt for testing.
 for ((i=0; i<${#URLS[@]}; ++i)); do
@@ -21,11 +38,12 @@ for ((i=0; i<${#URLS[@]}; ++i)); do
     echo "${DOMAIN}${ENDPOINT}" >> /etc/siege/urls.txt;
 done
 
-#Add host file configs if any.
 for ((i=0; i<${#HOSTS[@]}; ++i)); do
     HOST=${HOSTS[$i]}
     echo "${HOST}" >> /etc/hosts;
 done
+
+cat /etc/hosts
 
 siege -V
 siege "$@" 
