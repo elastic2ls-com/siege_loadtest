ed977e03ba72ed36db963f1b8636967706078715
Jonathan Costello <jon@weebly.com>
2015-01-08 11:18:56 -0800

    updated entry.sh to fix a function

--- entry.sh
+++ entry.sh
@@ -2,8 +2,8 @@
 
 # variables
 IFS="|"
-URLS=${URL_LIST}
-HOSTS=${ETC_HOSTS}
+URLS=(${URL_LIST})
+HOSTS=(${ETC_HOSTS})
 
 # configure login
 if ${USE_LOGIN}; then
@@ -21,12 +21,12 @@ cat /etc/siege/siegerc | grep -v '^#'
 #Add urls to urls.txt for testing.
 for ((i=0; i<${#URLS[@]}; ++i)); do
     ENDPOINT=${URLS[$i]}
-    echo "${DOMAIN}${ENDPOINT}" >> /etc/siege/urls.txt;
+    echo "${DOMAIN}${ENDPOINT}" >> /etc/siege/urls.txt
 done
 
 for ((i=0; i<${#HOSTS[@]}; ++i)); do
     HOST=${HOSTS[$i]}
-    echo "${HOST}" >> /etc/hosts;
+    echo "${HOST}" >> /etc/hosts
 done
 
 cat /etc/hosts
